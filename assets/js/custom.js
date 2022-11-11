b6c6d230b21749ee773dba92995c9c5b7668b11c
elomar1 <ahmedomarabdallah1@gmail.com>
2022-11-11 16:36:41 +0200

    home page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,12 @@
 
 
 $(document).ready(function () {
+
+
+
+
+
+
     // swiper home banner
     let swiper = new Swiper(".homeSwiper", {
         pagination: {
@@ -65,6 +71,68 @@ $(document).ready(function () {
     // return false;
   });
 
+      // Custom function which toggles between sticky class (is-sticky)
+      var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
+        var stickyHeight = sticky.outerHeight();
+        var stickyTop = stickyWrapper.offset().top;
+        if (scrollElement.scrollTop() >= stickyTop) {
+            stickyWrapper.height(stickyHeight);
+            sticky.addClass("is-sticky");
+        }
+        else {
+            sticky.removeClass("is-sticky");
+            stickyWrapper.height('auto');
+        }
+    };
 
+    // Find all data-toggle="sticky-onscroll" elements
+    $('[data-toggle="sticky-onscroll"]').each(function () {
+        var sticky = $(this);
+        var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+        sticky.before(stickyWrapper);
+        sticky.addClass('sticky');
+
+        // Scroll & resize events
+        $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
+            stickyToggle(sticky, stickyWrapper, $(this));
+        });
+
+        // On page load
+        stickyToggle(sticky, stickyWrapper, $(window));
+    });
 // end document ready
 });
+
+if($('.emdadNum').length ){
+  var counterUp = window.counterUp["default"]; // import counterUp from "counterup2"
+
+  var $counters = $(".counter");
+
+  /* Start counting, do this on DOM ready or with Waypoints. */
+  $counters.each(function (ignore, counter) {
+    var waypoint = new Waypoint({
+      element: $(this),
+      handler: function () {
+        counterUp(counter, {
+          duration: 2000,
+          delay: 10,
+        });
+        // this.destroy();
+      },
+      offset: "bottom-in-view",
+    });
+  });
+}
+
+  if($('.homeClassParent').length ){
+    $('.menuCustom').addClass('homeClass')
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+  
+      if (scroll >= 100) {
+          $(".menuCustom ").removeClass("homeClass");
+      } else {
+          $(".menuCustom ").addClass("homeClass");
+      }
+  });
+ }
